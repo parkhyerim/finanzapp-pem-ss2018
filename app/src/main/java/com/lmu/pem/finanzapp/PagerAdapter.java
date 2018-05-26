84f162a0b4e271591a17163b9f069a0409cc0fb4
H. Park <hallohyerim@gmail.com>
2018-05-26 22:35:24 +0200

    tab menus and a quicksearch edittext

---
+++ app/src/main/java/com/lmu/pem/finanzapp/PagerAdapter.java
@@ -0,0 +1,45 @@
+package com.lmu.pem.finanzapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int numOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumerOfTabs){
+        super(fm);
+        this.numOfTabs = NumerOfTabs;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+
+            case 2:
+                Tab3 tab3 = new Tab3();
+                return tab3;
+
+            case 3:
+                Tab4 tab4 = new Tab4();
+                return tab4;
+
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
