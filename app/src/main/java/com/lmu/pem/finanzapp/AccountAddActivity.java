017d414d8f7d02311b70b3c9d0a3be7ea793c205
Jan Obernolte <obernolte@cip.ifi.lmu.de>
2018-06-09 00:55:08 +0200

    fixed Singleton implementation of DashboardManager & AccountManager, EditText errors in AccountAddActivity

--- app/src/main/java/com/lmu/pem/finanzapp/AccountAddActivity.java
+++ app/src/main/java/com/lmu/pem/finanzapp/AccountAddActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -22,31 +24,48 @@ public class AccountAddActivity extends AppCompatActivity {
         addAccButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText nameView = findViewById(R.id.nameEdit);
+                final EditText nameView = findViewById(R.id.nameEdit);
                 String name = nameView.getText().toString();
 
-                if(AccountManager.getInstance().isNameTaken(name)){
-                    nameView.setBackgroundColor(0x70af0000);
-                    final Snackbar sb = Snackbar.make(v, "There is already an account with this name! Please insert a diffferent one.", Snackbar.LENGTH_LONG);
-                    sb.setAction("OK", new View.OnClickListener() {
+                if(name.length()<=0) {
+                    nameView.setError("You have to insert a name for the account.");
+                    nameView.addTextChangedListener(new TextWatcher() {
                         @Override
-                        public void onClick(View v) {
-                            sb.dismiss();
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                            nameView.setError(null);
+                            nameView.removeTextChangedListener(this);
                         }
                     });
-                    sb.show();
-                }else if(name.length()<=0) {
-                    nameView.setBackgroundColor(0x70af0000);
-                    final Snackbar sb = Snackbar.make(v, "You have to insert a name for the account.", Snackbar.LENGTH_LONG);
-                    sb.setAction("OK", new View.OnClickListener() {
+                }else if(AccountManager.getInstance().isNameTaken(name)){
+                    nameView.setError("There is already an account with this name");
+                    nameView.addTextChangedListener(new TextWatcher() {
                         @Override
-                        public void onClick(View v) {
-                            sb.dismiss();
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                            nameView.setError(null);
+                            nameView.removeTextChangedListener(this);
                         }
                     });
-                    sb.show();
                 }else{
-                    nameView.setBackgroundColor(0x00000000);
                     EditText balanceView = findViewById(R.id.balanceEdit);
                     double balance = 0;
                     try {
